40bf853e4310de3eee8210fa8edf0f407c941046
Bastiaan Quast <bquast@gmail.com>
2015-06-09 10:08:18 +0200

    remove author, renaming

--- R/wiod.R
+++ R/wiod.R
@@ -1,17 +1,15 @@
 #' @name wiod
 #' @docType package
 #' @title World Input Output Database 1995-2011
-#' @author
-#' Bastiaan Quast \email{bquast@@gmail.com}
 #' @seealso http://wwww.wiod.org/ http://qua.st/wiod
 #' @references {Timmer, Marcel P. (ed) (2012), "The World Input-Output Database (WIOD): Contents Sources and Methods", WIOD Working Paper Number 10, downloadable at http://www.wiod.org/publications/papers/wiod10.pdf }
 NULL
-#' @name final
+#' @name final95
 #' @docType data
 #' @title WIOD 1995 final
 #' @description WIOD 1995 final demand data
 NULL
-#' @name inter
+#' @name inter95
 #' @docType data
 #' @title WIOD 1995 inter
 #' @description WIOD 1995 intermediate demand data
@@ -26,7 +24,7 @@ NULL
 #' @title WIOD industries
 #' @description the names of the industries
 NULL
-#' @name out
+#' @name output95
 #' @docType data
 #' @title WIOD 1995 output
 #' @description WIOD 1995 final output
